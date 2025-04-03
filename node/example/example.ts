0e0bc2896539197164adb8adcb26fa2850f4b34a
Dief Bell <diefenbaker.bell@gmail.com>
2025-04-03 23:02:16 +0100

    Removed old test stuff

--- node/example/example.ts
+++ node/example/example.ts
@@ -67,30 +67,6 @@ const main = async () =>
 			}
 		}
 	}
-
-	// const file = files.find((file) => file.filename.includes("destruction"));
-	// if(!file) {
-	// 	throw new Error("Couldn't find file!");
-	// }
-
-	// const bitmap = file.bitmaps[0];
-	// const images = bitmap.images.filter((image) => !!image.parent);
-	// const image = images[10];
-
-	// const expectedFilename555 = bitmap.sgFilename.replace(".sg3", ".555");
-
-	// const { width, height, data } = new SgImageData(image, expectedFilename555);
-	// const s = sharp(
-	// 	data,
-	// 	{
-	// 		raw: { width, height, channels: 4 /* RGBA */}
-	// 	}
-	// );
-
-	// const filepath = path.join(outputFolder,
-	// 	`${path.basename(expectedFilename555, ".555")}_${bitmap.bitmapId}_${image.imageId}.png`
-	// );
-	// await s.toFile(filepath);
 }
 
 main();
